a084e0fec817ab47797ea5eb014c6b68150e4ac5
Praneeth Pidugu <pidugu2007@gmail.com>
2022-12-28 00:12:55 -0500

    Exploring JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,11 +3,58 @@
 console.log('app.js is running');
 
 var template = React.createElement(
-  "p",
-  null,
-  "This is JSX from app.js!"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info!"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item One"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item Two"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item Three"
+        )
+    )
 );
 
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Praneeth Pidugu"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 32"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Philadelphia"
+    )
+);
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,24 @@
 console.log('app.js is running');
 
-var template = <p>This is JSX from app.js!</p>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info!</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+            <li>Item Three</li>
+        </ol>
+    </div>
+    );
 
+var templateTwo = (
+    <div>
+        <h1>Praneeth Pidugu</h1>
+        <p>Age: 32</p>
+        <p>Location: Philadelphia</p>
+    </div>
+);
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
